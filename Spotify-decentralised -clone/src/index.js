1688eba80f99bf09ce576ce827f6a2f85efc2fba
Mat-Fraser <86045021+Mat-Fraser@users.noreply.github.com>
2022-09-08 12:16:02 +0530

    Add files via upload

---
+++ Spotify-decentralised -clone/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+import { MoralisProvider } from "react-moralis";
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MoralisProvider appId="aj8l8VnL9RHJwKZcWWctNUihPbGv0HBHeaBQ7FFx" serverUrl="https://9l7w4tznc9bp.usemoralis.com:2053/server">
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </MoralisProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
